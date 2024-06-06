7f304051f62859f6239b59393d7cbfff867e2a1f
jufu87 <168488450+jufu87@users.noreply.github.com>
2024-06-06 15:37:42 +0200

    Create operate function
    Create a new function operate that takes an operator and 2 numbers
    and then calls one of the above functions on the numbers.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let num1 = 20;
+let operator = "*";
+let num2 = 4;
+
 const add = function(num1, num2) {
     return num1 + num2;
 };
@@ -14,4 +18,21 @@ const divide = function(num1, num2) {
     return num1 / num2;
 }
 
-console.log(divide(10,5));
+
+const operate = function(num1, operator, num2) {
+    // console.log(num1, operator, num2);
+    if (operator === "+") {
+        console.log(add(num1, num2));
+    } else if (operator === "-") {
+        console.log(subtract(num1, num2));
+    } else if (operator === "*") {
+        console.log(multiply(num1,num2));
+    } else if (operator === "/") {
+        console.log(divide(num1, num2));
+    } else {
+        return;
+    }
+};
+
+//divide();
+operate(num1, operator, num2);
