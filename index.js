366cdbd530b50667d38ed635f517162d27787911
jufu87 <168488450+jufu87@users.noreply.github.com>
2024-06-06 18:17:15 +0200

    Add function to concat the display
    Add ability to concat the display unless it has the default value.

--- index.js
+++ index.js
@@ -22,22 +22,40 @@ let operator = "";
 let num2;
 let displayValue;
 
+// the buttons need to be differentiated between numbers, operators, and = or Clear
+
+// sotre first num variable when operator is clicked.add
+// After: when new num is clicked store in second variable with equals button=
+
+
+
 btns.forEach(function (i) {
     i.addEventListener('click', function() {
-
+        if (resultDisplay.value === "default") {
+            resultDisplay.value = "";
+        };
         console.log(i.textContent);
-        // store the value of the button into the num variable
+        // this stores the value of the button into the num variable
         // find a way to store the second and third button click in new variables
         num1 = i.textContent;
         console.log(num1);
         // when button is clicked, display the button's value in display and store it in new variable
-        // if variable already has value, store in next variable?
-        resultDisplay.value = i.textContent;
-        displayValue = parseInt(i.textContent);
+        // if variable already has value, store in next variable? doesnt work with non-single digit values
+        // so instead, if operator variable already has a value, store in new variable
+        
+
+      
+        updateDisplay(i.textContent)
     });
   });
 
+let updateDisplay = function(i) {
 
+    console.log(i);
+    resultDisplay.value += i; // need to concat the buttons pressed
+    displayValue = parseInt(i);
+    return i;
+}; //to run the resultDisplay.value and displayValue methods in if possible
 
 const add = function(num1, num2) {
     return num1 + num2;
